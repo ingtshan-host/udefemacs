4a742eda2b73219b904d78fc3ac900a90b511934
ingtshan <rongcanyo@gmail.com>
2021-10-29 15:43:32 +0800

    basic init

---
+++ init.el
@@ -0,0 +1,44 @@
+;;; init.el -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; init sequence to launch my emacs
+
+;;; Code:
+
+(when (version< emacs-version "26.1")
+  (error "This requires Emacs 26.1 and above!"))
+
+;; -1 for disable
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq
+ make-backup-files nil ; no file~
+ auto-save-default nil ; no #file#
+ 
+ ;; because I only keep one emacs process on one work dir
+ create-lockfiles nil) ; no .#file (for file lock)
+
+;; encoding
+;; UTF-8 as the default coding system
+(when (fboundp 'set-charset-priority)
+  (set-charset-priority 'unicode))
+
+;; to for add load-path recursion like -r
+(unless (boundp 'add-subdirs-to-load-path)
+  (defun add-subdirs-to-load-path(dir)
+    "Recursive add directories to `load-path`."
+    (let ((default-directory (file-name-as-directory dir)))
+      (add-to-list 'load-path dir)
+      (normal-top-level-add-subdirs-to-load-path))))
+
+;; stop emacs automatically editing .emacs
+(setq custom-file (expand-file-name "var/custom.el" user-emacs-directory))
+
+;; load init lib
+(add-subdirs-to-load-path
+   (expand-file-name "lisp" user-emacs-directory))
+
+;;; init.el ends here
